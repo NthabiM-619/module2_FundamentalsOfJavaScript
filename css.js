9ee60d7da3b6d658ade110655c7d9325d1c3f09f
Nthabiseng Mogoane <76831081+NthabiM-619@users.noreply.github.com>
2022-09-24 13:45:55 +0200

    css.js File
    Rolling the Dice

---
+++ css.js
@@ -0,0 +1,57 @@
+<style>
+	.container {
+		width: 70%,
+		"margin: auto"
+		"text-align: center
+	}
+
+	.dice {
+		"text-align: center",
+		"display: inline-block",
+		"margin: 10px"
+	}
+
+	body {
+		"background-color: #fe28a2",
+		"margin: 0"
+	}
+
+	h1 {
+		"margin: 30px",
+		"font-family: Lobster, cursive",
+		"text-shadow: 5px 0 #009966",
+		"font-size: 4.5rem",
+		"color: #77cc4e",
+		"text-align: center"
+	}
+
+	p {
+		"font-size: 2rem",
+		"color: #77cc4e",
+		"font-family: Indie Flower, cursive"
+	}
+
+	img {
+		"width: 100%"
+	}
+
+	.bottom {
+		"padding-top: 30px"
+	}
+
+	.btn {
+		"background: #c4e77c",
+		"font-family: Indie Flower, cursive",
+		"border-radius: 7px",
+		"color: #ede3d8",
+		"font-size: 30px",
+		"padding: 16px 25px 16px 25px",
+		"text-decoration: none"
+	}
+
+	.btn:hover {
+		"background:#c4e77c",
+
+		"text-decoration: none"
+	}
+</style>
